65c54580afe6fd64f14d2741450b56877bf86dab
VincentVazquez86 <7vvazquez@stpatrick.org>
2016-11-18 10:54:03 -0600

    Add files via upload

--- pizza.js
+++ pizza.js
@@ -1,13 +1,34 @@
 class pizza{
   //type constructor below
-this.size=XL;
-this.meatToppings=Sausage;
-this.veggieToppings=hotpeppers;
-size cost (){
+  constructor(s,mt,vt){
+    this.size=s;
+    this.meatToppings=mt;
+    this.veggieToppings=vt;
+  }
+sizecost(){
+if(this.size=="S"){
+  return 7.99
+}
+if(this.size=="M"){
+  return 9.99
+}
+if(this.size=="L"){
+  return 12.99
+}
+if(this.size=="XL"){
+  return 15.99
+}
+toppingcost(top){
+  if(top==meat){
+    return this.meatToppings.length*0.99;
+  }
+if(top==veggie){
+  else if this.veggieToppings.length*0.50;
+  }
+}
 
-
-
-  //type class functions below
+Price(){
+    //type class functions below
 
 
 }
