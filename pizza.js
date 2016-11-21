41b9a92dffe6aef2a72e61c0159ad1f1dc408c65
VincentVazquez86 <7vvazquez@stpatrick.org>
2016-11-21 11:45:01 -0600

    Add files via upload

--- pizza.js
+++ pizza.js
@@ -19,16 +19,21 @@ if(this.size=="XL"){
   return 15.99
 }
 toppingcost(top){
-  if(top==meat){
+  if(top=="meat"){
     return this.meatToppings.length*0.99;
   }
-if(top==veggie){
+if(top=="veggie"){
   else if this.veggieToppings.length*0.50;
   }
 }
 
 Price(){
-    //type class functions below
+    return this.sizeCost()+this.toppingCost("meat")+this.toppingCost("veggie");
+}    //type class functions below
 
+static promotionalDeal (pizza,percent){
+  percent = percent/100;
+  var discount = pizza.price()*percent;
+  return pizza.price() - discount;
 
 }
